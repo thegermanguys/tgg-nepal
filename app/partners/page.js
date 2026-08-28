426f443acd72e793591bb89795c80ab0fe72baad
Awanish Srivastava <40716261+imawanish5@users.noreply.github.com>
2026-08-28 21:25:22 +0200

    Add internet and SIM card options to partners page

--- app/partners/page.js
+++ app/partners/page.js
@@ -67,6 +67,23 @@ const Page = () => {
         btn_label: "Explore Plans",
       },
     ],
+    "Internet/Sim Card": [
+      {
+        name: "Get your German eSim",
+        url: "https://www.connectivityglobal.com/local-sim-alemania/?connecvitiy_ref=TheGermanGuy",
+        btn_label: "Order Now",
+      },
+      {
+        name: "SIM.DE",
+        url: "https://h.sim.de/12263fbdc468d?kw=a3c6MjI1YTcwYmMtYWMzNC00ZDk4LTkwNGMtYWE4MjU3ZTkwZTQ0",
+        btn_label: "Order Now",
+      },
+      {
+        name: "1&1",
+        url: "https://www.1und1.de/?ps_id=P786342813",
+        btn_label: "Order Now",
+      },
+    ],
     "German Banks": [
       {
         name: "N26",
